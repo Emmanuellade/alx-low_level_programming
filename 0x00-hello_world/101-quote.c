468370beddd2dffdad9dc73d5e59aa71c75becf0
emmanuella <emmanuellaadesida@gmail.com>
2023-04-21 04:46:41 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int len = sizeof(str) - 1;
+
+    write(2, str, len);
+
+    return (1);
+}
