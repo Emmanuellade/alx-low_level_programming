020cd3193dde9451483a8901b2e215d7b14e8bb3
emmanuella <emmanuellaadesida@gmail.com>
2023-04-21 05:44:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = sizeof(str) - 1;
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = sizeof(str) - 1;
 
-    write(2, str, len);
+write(2, str, len);
 
-    return (1);
+return (1);
 }
