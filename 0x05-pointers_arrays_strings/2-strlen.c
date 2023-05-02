4f0aa12ddef1ddae3ca0324830b175c7c3e79a08
emmanuella <emmanuellaadesida@gmail.com>
2023-05-02 15:20:28 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+
+	}
+
+	return (longi);
+
+}
