ee5c1967a5b64525f41497600285260ea105c387
emmanuella <emmanuellaadesida@gmail.com>
2023-05-22 15:23:05 -0700

    02-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it is used to prins file name that was compiled
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
